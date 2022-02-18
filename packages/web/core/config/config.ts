7fc4b29b11283690be7acf6c5e22e099e242e331
Jeffrey Lewis <jeffrey.lewis@iproperty.com.my>
2022-02-18 20:30:53 +0800

    Increase prepaid gas

--- packages/web/core/config/config.ts
+++ packages/web/core/config/config.ts
@@ -7,5 +7,6 @@ export interface AppConfig {
 
 export const Config: AppConfig = {
   nftContractName: 'nft.jeffreylewis.testnet',
-  gasFee: Big(3).times(10 ** 13).toFixed(),
+  // gasFee: Big(3).times(10 ** 14).toFixed(), // 300TGas
+  gasFee: Big(1).times(10 ** 14).toFixed(), // 100TGas
 };
